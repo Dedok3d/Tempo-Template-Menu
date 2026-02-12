7b4cfee20667aad97353b209429a757b7fb5eec8
Dedok3d <Sersh_96@mail.ru>
2026-02-12 13:54:22 +0700

    version up

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Tempo Template Menu
 // @namespace https://jira.russianpost.ru/
-// @version 0.1
+// @version 0.2
 // @match *://jira.russianpost.ru/browse/*
 // @match *://jora.ru/secure/Tempo.jspa*
 // @grant none
